60a46b19a9377638cfa4bd3229a6629c4a19fe3d
Corrin704 <Williammvutu@gmail.com>
2026-02-10 00:35:04 -0500

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,28 @@
+const GALLERY = {
+  music: {
+    title: "Music Videos",
+    subtitle: "Selected music video stills",
+    images: [
+      "https://images.unsplash.com/photo-1531297484001-80022131f5aW9waWVkaQ",
+      "https://images.unsplash.com/photo-1517602302552-471fe67acf66",
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728"
+    ]
+  },
+
+  documentary: {
+    title: "Documentary",
+    subtitle: "Natural and controlled color",
+    images: [
+      "https://images.unsplash.com/photo-1526948128573-703ee1aeb6fa",
+      "https://images.unsplash.com/photo-1505238680356-667803448bb6"
+    ]
+  },
+
+  narrative: {
+    title: "Narrative",
+    subtitle: "Story driven cinematic grading",
+    images: [
+      "https://images.unsplash.com/photo-1522125670776-3c7abb882bc2"
+    ]
+  }
+};
